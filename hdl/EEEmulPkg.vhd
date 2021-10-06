e3ab11757f97f13de284682717e4ae065ecfc55d
till straumann <till.straumann@alumni.tu-berlin.de>
2021-10-06 15:03:04 +0200

    EEEmulPkg.vhd: read 0000 from out-of-range addresses.
    Such addresses potentially come from the master and should not
    fail the simulation.

--- hdl/EEEmulPkg.vhd
+++ hdl/EEEmulPkg.vhd
@@ -47,11 +47,19 @@ package body EEEMulPkg is
       variable hival : out   std_logic_vector(31 downto 0)
    ) is
       variable val : std_logic_vector(63 downto 0);
+      variable idx : integer;
+      variable v16 : std_logic_vector(15 downto 0);
    begin
 --report "EEPROM READ @ " & integer'image(to_integer(unsigned(addr(15 downto 0))));
       for w in 0 to 3 loop
 --report "    " &  integer'image(to_integer(unsigned(eeprom( to_integer(unsigned(addr(15 downto 0))) + w ))));
-         val(w*16 + 15 downto w*16) := eep( to_integer(unsigned(addr(15 downto 0))) + w );
+         idx := to_integer(unsigned(addr(15 downto 0))) + w;
+         if ( idx <= eep'high ) then
+            v16 := eep( idx );
+         else
+            v16 := (others => '0');
+         end if;
+         val(w*16 + 15 downto w*16) := v16;
       end loop;
       hival := val(63 downto 32);
       loval := val(31 downto  0);
