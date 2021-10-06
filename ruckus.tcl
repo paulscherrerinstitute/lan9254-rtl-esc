2fd5caa2148ce7a139c40854e2c2bc1d8d04df8d
till straumann <till.straumann@alumni.tu-berlin.de>
2021-10-06 15:08:14 +0200

    ruckus.tcl: updated file list

--- ruckus.tcl
+++ ruckus.tcl
@@ -5,16 +5,32 @@ exec bash -c "make -C $::DIR_PATH/hdl EEPROMContentPkg.vhd"
 
 # Load local source Code and constraints
 foreach f {
-  Lan9254Pkg.vhd
-  Lan9254ESCPkg.vhd
-  EEEmulPkg.vhd
-  EEPROMContentPkg.vhd
-  Lan9254ESC.vhd
-  Lan9254Hbi.vhd
-  Lan9254HbiImpl.vhd
-  SynchronizerBit.vhd
+EEEmulPkg.vhd
+EEPROMContentPkg.vhd
+ESCEoERx.vhd
+ESCEoETx.vhd
+ESCMbxPkg.vhd
+ESCRxMbxMux.vhd
+ESCSmRx.vhd
+ESCTxMbxBuf.vhd
+ESCTxMbxErr.vhd
+ESCTxMbxMux.vhd
+ESCTxPDO.vhd
+IPV4ChkSum.vhd
+Lan9254ESCPkg.vhd
+Lan9254ESCrun.vhd
+Lan9254ESC.vhd
+Lan9254ESCWrapper.vhd
+Lan9254HbiImpl.vhd
+Lan9254Hbi.vhd
+Lan9254Pkg.vhd
+MicroUDPPkg.vhd
+MicroUDPRx.vhd
+MicroUDPTx.vhd
+SynchronizerBit.vhd  Lan9254Pkg.vhd
 } {
   loadSource    -path "$::DIR_PATH/hdl/$f"
 }
+
 
 
