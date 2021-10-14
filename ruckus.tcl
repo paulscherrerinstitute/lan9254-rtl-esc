e7d65847d29312f3296a3a52fb66ba826efa4e0e
till straumann <till.straumann@alumni.tu-berlin.de>
2021-10-14 09:14:33 +0200

    updated ruckus.tcl

--- ruckus.tcl
+++ ruckus.tcl
@@ -5,29 +5,31 @@ exec bash -c "make -C $::DIR_PATH/hdl EEPROMContentPkg.vhd"
 
 # Load local source Code and constraints
 foreach f {
-EEEmulPkg.vhd
-EEPROMContentPkg.vhd
-ESCEoERx.vhd
-ESCEoETx.vhd
-ESCMbxPkg.vhd
-ESCRxMbxMux.vhd
-ESCSmRx.vhd
-ESCTxMbxBuf.vhd
-ESCTxMbxErr.vhd
-ESCTxMbxMux.vhd
-ESCTxPDO.vhd
-IPV4ChkSum.vhd
-Lan9254ESCPkg.vhd
-Lan9254ESCrun.vhd
-Lan9254ESC.vhd
-Lan9254ESCWrapper.vhd
-Lan9254HbiImpl.vhd
-Lan9254Hbi.vhd
-Lan9254Pkg.vhd
-MicroUDPPkg.vhd
-MicroUDPRx.vhd
-MicroUDPTx.vhd
-SynchronizerBit.vhd  Lan9254Pkg.vhd
+  EEEmulPkg.vhd
+  EEPROMContentPkg.vhd
+  ESCEoERx.vhd
+  ESCEoETx.vhd
+  ESCMbxPkg.vhd
+  ESCRxMbxMux.vhd
+  ESCSmRx.vhd
+  ESCTxMbxBuf.vhd
+  ESCTxMbxErr.vhd
+  ESCTxMbxMux.vhd
+  ESCTxPDO.vhd
+  IPV4ChkSum.vhd
+  Lan9254ESCPkg.vhd
+  Lan9254ESCrun.vhd
+  Lan9254ESC.vhd
+  Lan9254ESCWrapper.vhd
+  Lan9254HbiImpl.vhd
+  Lan9254Hbi.vhd
+  Lan9254Pkg.vhd
+  MicroUDPPkg.vhd
+  MicroUDPRx.vhd
+  MicroUDPTx.vhd
+  SynchronizerBit.vhd
+  Lan9254Pkg.vhd
+  StrmFrameBuf.vhd
 } {
   loadSource    -path "$::DIR_PATH/hdl/$f"
 }
