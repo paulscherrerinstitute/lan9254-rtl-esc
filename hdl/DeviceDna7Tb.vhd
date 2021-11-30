9b4583e5ae3a99d04916bb9c57cdca46af8ec3a0
till straumann <till.straumann@alumni.tu-berlin.de>
2021-11-30 16:08:49 +0100

    DeviceDna7Tb.vhd: extended test; report valid flag after some clocks

--- hdl/DeviceDna7Tb.vhd
+++ hdl/DeviceDna7Tb.vhd
@@ -15,18 +15,37 @@ architecture sim of DeviceDna7Tb is
 
    signal dna : std_logic_vector(56 downto 0);
 
+   signal run : boolean   := true;
+
 begin
 
    P_CLK : process is
    begin
-      if ( vld = '1' ) then
-         report "DNA: " & toString( dna );
+      if ( not run ) then
          wait;
       else
          wait for 10 ns;
          clk <= not clk;
       end if;
    end process P_CLK;
+
+   P_MON : process is
+   begin
+      while ( vld /= '1' ) loop
+         wait until rising_edge( clk );
+      end loop;
+
+      report "DNA: " & toString( dna );
+
+      for i in 0 to 10 loop
+         wait until rising_edge( clk );
+      end loop;
+
+      report "VLD after 10 clocks: " & std_logic'image( vld );
+
+      run <= false;
+      wait;
+   end process P_MON;
 
    P_RST : process ( clk ) is
    begin
